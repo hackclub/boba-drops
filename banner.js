b1fd466524ab25bc91b08f18c8c3a9d5d092d5b0
WolfGamer2 <rushilchopra@gmail.com>
2024-11-12 12:50:06 -0800

    added the itnl boba banner!

---
+++ banner.js
@@ -0,0 +1,9 @@
+document.getElementById("announcement-banner").addEventListener("click", function() {
+    var additionalInfo = document.getElementById("additional-info");
+    if (additionalInfo.style.display === "block") {
+      additionalInfo.style.display = "none";
+    } else {
+      additionalInfo.style.display = "block";
+    }
+  });
+  
